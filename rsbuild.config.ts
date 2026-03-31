867aaf60eb3caa57f710c19990288ee1b02df84c
Nestor Cortina <nexckycort@gmail.com>
2026-03-31 18:09:59 -0500

    feat: update dependencies and improve HTML template handling

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,17 +2,21 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { TanStackRouterRspack } from '@tanstack/router-plugin/rspack';
 
+const APP_ENV = process.env.PUBLIC_APP_ENV || 'prod';
+const isProd = APP_ENV === 'prod';
+
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   html: {
     template: './public/index.html',
+    templateParameters: {
+      TITLE: `Wallet ${isProd ? 'Bloque' : 'Dev'}`,
+    },
   },
   plugins: [pluginReact()],
   tools: {
     rspack: {
-      plugins: [
-        TanStackRouterRspack(),
-      ].filter(Boolean),
+      plugins: [TanStackRouterRspack()].filter(Boolean),
     },
   },
 });
